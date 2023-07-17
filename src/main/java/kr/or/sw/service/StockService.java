62b98d158223f65e7054bfc962949f959f255468
bnbu <jsh031613@naver.com>
2023-07-17 19:26:14 +0900

    feat: add product recipe
    delete selected data will add in future

--- src/main/java/kr/or/sw/service/StockService.java
+++ src/main/java/kr/or/sw/service/StockService.java
@@ -2,10 +2,13 @@ package kr.or.sw.service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public interface StockService extends Service {
 
     void selectAll(HttpServletRequest request, HttpServletResponse response);
 
     void searchBy(HttpServletRequest request, HttpServletResponse response);
+
+    void getStocks(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
